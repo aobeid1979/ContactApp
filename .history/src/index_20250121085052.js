f72673542d20a8691a7f90ce2c503641fae7cbc9
Amma Obeid <amfo250@gmail.com>
2025-01-26 21:55:30 +0100

    Initial commit

---
+++ .history/src/index_20250121085052.js
@@ -0,0 +1,17 @@
+
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import Header from './components/Header';
+import Counter from './components/Counter';
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+  <Header></Header>
+  
+  </div>
+);
+
